1baa5ce04b0a7d7fd6b1e78fccff03c2a9b8ec07
Admin <temrevil@gmail.com>
2026-02-16 22:56:13 +0200

    Rebranding

--- electron/preload.ts
+++
@@ -1,5 +0,0 @@
-import { contextBridge } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Add any IPC methods here if needed
-});
--- pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import App from '../src/App'
-import React from 'react'
-
-export default function Home() {
-  return <App />
-}
